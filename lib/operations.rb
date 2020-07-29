2fead281378fdf79b52d29c85368f87a16406dcd
Stephen Junge <steve.junge@gmail.com>
2020-07-29 19:16:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 require 'pry'
 
 def unsafe?(speed)
-  if speed < 40 or > 60  
+  if speed  > 60  
+    return "true"
+  if speed < 40
     return "true"
   else
     return "false"
 end
-
+end
 
 
 def not_safe?(speed)
