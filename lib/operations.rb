28f5a45cdd2775405dbc7f99ab1c0a62af147736
Stephen Junge <steve.junge@gmail.com>
2020-07-29 20:37:27 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,7 @@ end
 
 
 def not_safe?(speed)
-	speed < 40 ? return true : return false
-	speed > 60 ? return true : return false
+	speed < 40 || > 60  ? return true : return false
 binding.pry
 end
 	
